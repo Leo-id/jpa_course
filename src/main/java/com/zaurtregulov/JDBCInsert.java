f2cb33223aa362d71985a7f14538b72ce27c8e8d
Константин Леончик <k.leonchik@inbox.ru>
2025-04-07 00:16:01 +0300

    first commit

---
+++ src/main/java/com/zaurtregulov/JDBCInsert.java
@@ -0,0 +1,11 @@
+package com.zaurtregulov;
+
+public class JDBCInsert {
+    static final String DB_URL="jdbc:mysql://localhost:3306/test_db";
+    static final String USER="jpauser";
+    static final String PWD="jpapwd";
+
+    public static void main(String[] args) {
+
+    }
+}
